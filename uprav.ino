3ce38f3045c768132e48c2f0b54069cee5e2c7bf
cmitKem42 <workcmit@gmail.com>
2014-11-04 14:42:13 +0600

    Стало работать переключение товаров

--- uprav.ino
+++ uprav.ino
@@ -1,58 +1,53 @@
 //Sample using LiquidCrystal library
 #include <LiquidCrystal.h>
-#include "Ultrasonic.h"
-
-
+#include <Ultrasonic.h>
 /*******************************************************
-
-This program will test the LCD panel and the buttons
-Mark Bramwell, July 2010
-
-********************************************************/
-
+ * This program will test the LCD panel and the buttons
+ * Mark Bramwell, July 2010
+ ********************************************************/
 // select the pins used on the LCD panel
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 Ultrasonic ultrasonic(50, 52);
 // define some values used by the panel and buttons
 const byte buzzPin = 32;
-int lcd_key     = 0;
-int adc_key_in  = 0;
-#define btnRIGHT  0
-#define btnUP     1
-#define btnDOWN   2
-#define btnLEFT   3
+int lcd_key = 0;
+int adc_key_in = 0;
+#define btnRIGHT 0
+#define btnUP 1
+#define btnDOWN 2
+#define N 4
+#define btnLEFT 3
 #define btnSELECT 4
-#define btnNONE   5
+#define btnNONE 5
 int i=0;
-char data[][10] = {"Beer", "Bread", "Cola", "Soda"};
+char data[][10] = {
+  "Beer", "Bread", "Cola", "Soda"};
 // read the buttons
 int read_LCD_buttons()
 {
- adc_key_in = analogRead(0);      // read the value from the sensor 
- // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
- // we add approx 50 to those values and check to see if we are close
- if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
- // For V1.1 us this threshold
- if (adc_key_in < 50)   return btnRIGHT;  
- if (adc_key_in < 250)  return btnUP; 
- if (adc_key_in < 450)  return btnDOWN; 
- if (adc_key_in < 650)  return btnLEFT; 
- if (adc_key_in < 850)  return btnSELECT;  
-
- // For V1.0 comment the other threshold and use the one below:
-/*
- if (adc_key_in < 50)   return btnRIGHT;  
- if (adc_key_in < 195)  return btnUP; 
- if (adc_key_in < 380)  return btnDOWN; 
- if (adc_key_in < 555)  return btnLEFT; 
- if (adc_key_in < 790)  return btnSELECT;   
-*/
-
-
- return btnNONE;  // when all others fail, return this...
+  adc_key_in = analogRead(0); // read the value from the sensor
+  // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+  // we add approx 50 to those values and check to see if we are close
+  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+  // For V1.1 us this threshold
+  if (adc_key_in < 50) return btnRIGHT;
+  if (adc_key_in < 250) return btnUP;
+  if (adc_key_in < 450) return btnDOWN;
+  if (adc_key_in < 650) return btnLEFT;
+  if (adc_key_in < 850) return btnSELECT;
+  // For V1.0 comment the other threshold and use the one below:
+  /*
+if (adc_key_in < 50) return btnRIGHT;
+   if (adc_key_in < 195) return btnUP;
+   if (adc_key_in < 380) return btnDOWN;
+   if (adc_key_in < 555) return btnLEFT;
+   if (adc_key_in < 790) return btnSELECT;
+   */
+  return btnNONE; // when all others fail, return this...
 }
 void dis(){
-  {beep(50);
+  {
+    beep(50);
     beep(50);
     beep(50);
     delay(100);
@@ -63,79 +58,90 @@ void dis(){
     beep(50);
     beep(50);
     delay(1000);
- }
+  }
 }
-
 void setup()
 {
- lcd.begin(16, 2);              // start the library
- lcd.setCursor(0,0);
- //lcd.print("dist"); // print a simple message
- pinMode(buzzPin,OUTPUT);
- 
+  Serial.begin(9600);
+  lcd.begin(16, 2); // start the library
+  lcd.setCursor(0,0);
+  //lcd.print("dist"); // print a simple message
+  pinMode(buzzPin,OUTPUT);
 }
- 
- void beep( int delt ){
-    tone(buzzPin, 2093);
-    delay(delt);
-    noTone(buzzPin);
-    delay(delt);
+void beep( int delt ){
+  tone(buzzPin, 2093);
+  delay(delt);
+  noTone(buzzPin);
+  delay(delt);
+}
+void printElement(int i)
+{
+          Serial.print(i);
+          lcd.clear();
+          lcd.print(data[i]);
 }
 void loop()
 {
-  
   float dist_cm = ultrasonic.Ranging(CM);
   if(dist_cm==10)dis();
- lcd.setCursor(9,1);            // move cursor to second line "1" and 9 spaces over
- lcd.print(dist_cm);      // display seconds elapsed since power-up
+  lcd.setCursor(9,1); // move cursor to second line "1" and 9 spaces over
+  lcd.print(dist_cm); // display seconds elapsed since power-up
+  //lcd.clear();
+  lcd.setCursor(0,1); // move to the begining of the second line
+  lcd_key = read_LCD_buttons(); // read the buttons
+  switch (lcd_key) // depending on which button was pushed, we perform an action
+  {
+  case btnRIGHT:
+    {
+      lcd.print("RIGHT ");
+      break;
+    }
+  case btnLEFT:
+    {
+      lcd.print("LEFT ");
+      break;
+    }
+  case btnUP:
+    {
+      //if (i > N - 1) i--;
+      //else{
+        if(i == N - 1){
+          printElement(i);
+        }
+        else{
+                    
+          printElement(i);
+          i++;
 
-lcd.clear();
- lcd.setCursor(0,1);            // move to the begining of the second line
- lcd_key = read_LCD_buttons();  // read the buttons
+         }
+      delay(300);
+      break;
+    }
+  case btnDOWN:
+    {
+      if (i == 0) printElement(i);
+      else{
+              //Serial.print(i);
 
- switch (lcd_key)               // depending on which button was pushed, we perform an action
- {
-   case btnRIGHT:
-     {
-     lcd.print("RIGHT ");
-     break;
-     }
-   case btnLEFT:
-     {
-     lcd.print("LEFT   ");
-     break;
-     }
-   case btnUP:
-     {
-     lcd.print(data[i]);
-     i++;
-     
-     
-     delay(1500);
-     
-     break;
-     }
-   case btnDOWN:
-     {
-       lcd.print(data[i]);
-     i--;
-     
-     delay(1500);
-     
+                i--;
+        printElement(i);
 
-     break;
-     }
-   case btnSELECT:
-     {
-     //if(i==10){lcd.print("ok"); dis();}else lcd.print("no");
-     break;
-     }
-     case btnNONE:
-     {
-           // 
-     //lcd.print("NONE  ");
-     break;
-     }
- }
-
+      }
+      delay(300);
+      break;
+    }
+  case btnSELECT:
+    {
+      //if(i==10){lcd.print("ok"); dis();}else lcd.print("no");
+      //i = 0;
+      break;
+    }
+  case btnNONE:
+    {
+      //i = 0;
+      //
+      //lcd.print("NONE ");
+      break;
+    }
+  }
 }
